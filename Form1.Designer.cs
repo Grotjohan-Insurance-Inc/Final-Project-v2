8a2124f2f4bae9a27938c61082dff0bf66f7f713
Chris4285 <69996380+Chris4285@users.noreply.github.com>
2022-01-14 13:16:17 -0500

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Final_Project
+﻿using System;
+
+namespace Final_Project
 {
     partial class Form1
     {
@@ -28,33 +30,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.txtNumber1 = new System.Windows.Forms.TextBox();
             this.txtNumber2 = new System.Windows.Forms.TextBox();
             this.txtOutput = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
-            this.pictureBox7 = new System.Windows.Forms.PictureBox();
-            this.pictureBox8 = new System.Windows.Forms.PictureBox();
-            this.pictureBox9 = new System.Windows.Forms.PictureBox();
             this.txtGridSize = new System.Windows.Forms.TextBox();
-            this.txtGridLocation = new System.Windows.Forms.TextBox();
             this.btnCalculate = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.btnUpload = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
             this.SuspendLayout();
             // 
             // txtNumber1
@@ -73,7 +59,7 @@
             // 
             // txtOutput
             // 
-            this.txtOutput.Location = new System.Drawing.Point(197, 260);
+            this.txtOutput.Location = new System.Drawing.Point(197, 201);
             this.txtOutput.Name = "txtOutput";
             this.txtOutput.Size = new System.Drawing.Size(202, 31);
             this.txtOutput.TabIndex = 3;
@@ -81,92 +67,12 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
-            this.pictureBox1.Location = new System.Drawing.Point(947, 31);
+            this.pictureBox1.Location = new System.Drawing.Point(985, 70);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.Size = new System.Drawing.Size(759, 763);
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.BackColor = System.Drawing.SystemColors.Control;
-            this.pictureBox3.Location = new System.Drawing.Point(1153, 31);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(200, 200);
-            this.pictureBox3.TabIndex = 7;
-            this.pictureBox3.TabStop = false;
-            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
-            this.pictureBox2.Location = new System.Drawing.Point(1359, 31);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(200, 200);
-            this.pictureBox2.TabIndex = 8;
-            this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.BackColor = System.Drawing.SystemColors.Control;
-            this.pictureBox4.Location = new System.Drawing.Point(1359, 237);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(200, 200);
-            this.pictureBox4.TabIndex = 11;
-            this.pictureBox4.TabStop = false;
-            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
-            // 
-            // pictureBox5
-            // 
-            this.pictureBox5.BackColor = System.Drawing.SystemColors.Control;
-            this.pictureBox5.Location = new System.Drawing.Point(1153, 237);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(200, 200);
-            this.pictureBox5.TabIndex = 10;
-            this.pictureBox5.TabStop = false;
-            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
-            // 
-            // pictureBox6
-            // 
-            this.pictureBox6.BackColor = System.Drawing.SystemColors.Control;
-            this.pictureBox6.Location = new System.Drawing.Point(947, 237);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(200, 200);
-            this.pictureBox6.TabIndex = 9;
-            this.pictureBox6.TabStop = false;
-            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
-            // 
-            // pictureBox7
-            // 
-            this.pictureBox7.BackColor = System.Drawing.SystemColors.Control;
-            this.pictureBox7.Location = new System.Drawing.Point(1359, 456);
-            this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(200, 200);
-            this.pictureBox7.TabIndex = 14;
-            this.pictureBox7.TabStop = false;
-            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
-            // 
-            // pictureBox8
-            // 
-            this.pictureBox8.BackColor = System.Drawing.SystemColors.Control;
-            this.pictureBox8.Location = new System.Drawing.Point(1153, 456);
-            this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(200, 200);
-            this.pictureBox8.TabIndex = 13;
-            this.pictureBox8.TabStop = false;
-            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
-            // 
-            // pictureBox9
-            // 
-            this.pictureBox9.BackColor = System.Drawing.SystemColors.Control;
-            this.pictureBox9.Location = new System.Drawing.Point(947, 456);
-            this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(200, 200);
-            this.pictureBox9.TabIndex = 12;
-            this.pictureBox9.TabStop = false;
-            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
             // 
             // txtGridSize
             // 
@@ -174,13 +80,6 @@
             this.txtGridSize.Name = "txtGridSize";
             this.txtGridSize.Size = new System.Drawing.Size(684, 31);
             this.txtGridSize.TabIndex = 15;
-            // 
-            // txtGridLocation
-            // 
-            this.txtGridLocation.Location = new System.Drawing.Point(197, 200);
-            this.txtGridLocation.Name = "txtGridLocation";
-            this.txtGridLocation.Size = new System.Drawing.Size(580, 31);
-            this.txtGridLocation.TabIndex = 16;
             // 
             // btnCalculate
             // 
@@ -195,66 +94,56 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 260);
+            this.label1.Location = new System.Drawing.Point(19, 201);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(134, 25);
             this.label1.TabIndex = 18;
             this.label1.Text = "Aspect Ratio";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 206);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(140, 25);
-            this.label2.TabIndex = 19;
-            this.label2.Text = "Grid Location";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(19, 155);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(100, 25);
+            this.label3.Size = new System.Drawing.Size(102, 25);
             this.label3.TabIndex = 20;
-            this.label3.Text = "Grid Size";
+            this.label3.Text = "0.3 Scale";
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(24, 261);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(456, 78);
+            this.btnUpload.TabIndex = 21;
+            this.btnUpload.Text = "Upload your own image here!";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2431, 2080);
+            this.ClientSize = new System.Drawing.Size(2484, 1126);
+            this.Controls.Add(this.btnUpload);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnCalculate);
-            this.Controls.Add(this.txtGridLocation);
             this.Controls.Add(this.txtGridSize);
-            this.Controls.Add(this.pictureBox7);
-            this.Controls.Add(this.pictureBox8);
-            this.Controls.Add(this.pictureBox9);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox6);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.txtOutput);
             this.Controls.Add(this.txtNumber2);
             this.Controls.Add(this.txtNumber1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Aspect Ratio Custom Image Calculator";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
 
         }
 
@@ -264,20 +153,11 @@
         private System.Windows.Forms.TextBox txtNumber2;
         private System.Windows.Forms.TextBox txtOutput;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.PictureBox pictureBox6;
-        private System.Windows.Forms.PictureBox pictureBox7;
-        private System.Windows.Forms.PictureBox pictureBox8;
-        private System.Windows.Forms.PictureBox pictureBox9;
         private System.Windows.Forms.TextBox txtGridSize;
-        private System.Windows.Forms.TextBox txtGridLocation;
         private System.Windows.Forms.Button btnCalculate;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnUpload;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,128 +14,78 @@ namespace Final_Project
     {
         public Form1()
         {
+            // I wrote these as the first thing when executed because I wanted to purely for aesthetic purposes inside the box
             InitializeComponent();
-            txtGridLocation.Text = "Insert a Pixel Size to see it's size on the Grid!";
-            txtGridLocation.ForeColor = Color.Gray;
             txtOutput.Text = "Insert a Pixel Size to see the Aspect Ratio!";
             txtOutput.ForeColor = Color.Gray;
         }
-        
+        //This is my method in which when I press my buttons, both for uploading an image or just standalone calculating the aspect ratio
         public void AspectRatioCalculator()
         {
-
+            //Here I converted my text boxes to doubles because it was easier to use them in regards to using math
             double dNumber1 = Convert.ToDouble(txtNumber1.Text);
             double dNumber2 = Convert.ToDouble(txtNumber2.Text);
+            //Declared my Greatest common factor here and set it to 0
             double dGCF = 0;
+            //Used the math formula to calculate the minimum for the two numbers which will be used in our for loop to tell the loop to stop when the variable
+            //Is less than the minimum to avoid infinity
             double dMin = Math.Min(dNumber1, dNumber2);
-            double dNumber3 = 0;
-            double dNumber4 = 0;
-            double dNumber5 = 0;
-            double dNumber6 = 0;
+            //Declared my aspect ratio numbers which are stringed to show the aspect ratio in the output text box
+            double dAspectRatio1 = 0;
+            double dAspectRatio2 = 0;
+            //This is the reduced aspect ratio which is shown to the viewer at a reduced scale so the can visualize the photo
+            double dReducedAspectRatio1 = 0;
+            double dReducedAspectRatio2 = 0;
+            //This for loop loops everytime there until the variable is less than or equal to the Minimum
             for (int i = 0; i <= dMin; i++)
             {
+                // This if statement only works if both numbers are perfectly divisible and i is greater than 0
                 if ((dNumber1 % i == 0) && (dNumber2 % i == 0) && (i > 0))
                 {
+                    //Obviously this gives us our answer of the GCF
                     dGCF = i;
                 }
 
             }
-            dNumber3 = dNumber1 / dGCF;
-            dNumber4 = dNumber2 / dGCF;
-            txtOutput.Text = dNumber3 + ":" + dNumber4;
-            dNumber5 = dNumber1 / 3;
-            dNumber6 = dNumber2 / 3;
-
+            //From here the Aspect Ratio is the original number divided by the GCF
+            dAspectRatio1 = dNumber1 / dGCF;
+            dAspectRatio2 = dNumber2 / dGCF;
+            //And thus you string it here
+            txtOutput.Text = dAspectRatio1 + ":" + dAspectRatio2;
+            //The reeduces Aspect Ratio is the users original numbers divided by 3 so that it doesnt explode the image on the
+            //Window and can actually be viewable to get a visual of, hence the 0.3 scale.
+            dReducedAspectRatio1 = dNumber1 / 3;
+            dReducedAspectRatio2 = dNumber2 / 3;
+            //This is again for aesthetic purposes to change the colour back to black so it's not grey when presented the answer
             txtOutput.ForeColor = Color.Black;
-
-            txtGridSize.Text = dNumber5 + ":" + dNumber6;
-
+            //And strings over to the textbox
+            txtGridSize.Text = dReducedAspectRatio1 + ":" + dReducedAspectRatio2;
+            //This creates the box as dark grey if no image is selected
             pictureBox1.BackColor = Color.DarkGray;
-            pictureBox2.BackColor = Color.DarkGray;
-            pictureBox3.BackColor = Color.DarkGray;
-            pictureBox4.BackColor = Color.DarkGray;
-            pictureBox5.BackColor = Color.DarkGray;
-            pictureBox6.BackColor = Color.DarkGray;
-            pictureBox7.BackColor = Color.DarkGray;
-            pictureBox8.BackColor = Color.DarkGray;
-            pictureBox9.BackColor = Color.DarkGray;
-
-            Size RuleOfThirdsGraph = new Size((int)dNumber5, (int) dNumber6);
+            //And makes the picture box the reduced size
+            Size RuleOfThirdsGraph = new Size((int)dReducedAspectRatio1, (int)dReducedAspectRatio2);
             pictureBox1.Size = RuleOfThirdsGraph;
-            pictureBox2.Size = RuleOfThirdsGraph;
-            pictureBox3.Size = RuleOfThirdsGraph;
-            pictureBox4.Size = RuleOfThirdsGraph;
-            pictureBox5.Size = RuleOfThirdsGraph;
-            pictureBox6.Size = RuleOfThirdsGraph;
-            pictureBox7.Size = RuleOfThirdsGraph;
-            pictureBox8.Size = RuleOfThirdsGraph;
-            pictureBox9.Size = RuleOfThirdsGraph;
-
-            txtGridSize.Text = "This Pixel Size is " + dNumber5 + ":" + dNumber6 + ".";
+            //Then outputs this
+            txtGridSize.Text = "The Scaled Down Size shown is " + dReducedAspectRatio1 + ":" + dReducedAspectRatio2 + ".";
         }
-
-        public void GridPixelCalculator()
-        {
-            txtGridLocation.ForeColor = Color.Black;
-        }
-
+        //when the button is pressed it then calls back to the Aspect Ratio Calculator Method because I need the same math for this button
+        //with no image...
         private void btnCalculate_Click(object sender, EventArgs e)
         {
             AspectRatioCalculator();
         }
-
-        public void pictureBox1_Click(object sender, EventArgs e)
+        //and this button with an image
+        private void btnUpload_Click(object sender, EventArgs e)
         {
-            GridPixelCalculator();
-            txtGridLocation.Text = "This is the grid (1,1)";
-        }
+            AspectRatioCalculator();
+            //This line of code I learned through a course I took for C# which allows the user to upload a JPG or PNG to display their image in that aspect ratio
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JPG|*.jpg|PNG|*.png";
 
-        private void pictureBox3_Click(object sender, EventArgs e)
-        {
-            GridPixelCalculator();
-            txtGridLocation.Text = "This is the grid (2,1)";
-        }
-
-        private void pictureBox2_Click(object sender, EventArgs e)
-        {
-            GridPixelCalculator();
-            txtGridLocation.Text = "This is the grid (3,1)";
-        }
-
-        private void pictureBox6_Click(object sender, EventArgs e)
-        {
-            GridPixelCalculator();
-            txtGridLocation.Text = "This is the grid (1,2)";
-        }
-
-        private void pictureBox5_Click(object sender, EventArgs e)
-        {
-            GridPixelCalculator();
-            txtGridLocation.Text = "This is the grid (2,2)";
-        }
-
-        private void pictureBox4_Click(object sender, EventArgs e)
-        {
-            GridPixelCalculator();
-            txtGridLocation.Text = "This is the grid (3,2)";
-        }
-
-        private void pictureBox9_Click(object sender, EventArgs e)
-        {
-            GridPixelCalculator();
-            txtGridLocation.Text = "This is the grid (1,3)";
-        }
-
-        private void pictureBox8_Click(object sender, EventArgs e)
-        {
-            GridPixelCalculator();
-            txtGridLocation.Text = "This is the grid (2,3)";
-        }
-
-        private void pictureBox7_Click(object sender, EventArgs e)
-        {
-            GridPixelCalculator();
-            txtGridLocation.Text = "This is the grid (3,3)";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Bitmap.FromFile(openFileDialog.FileName);
+            }
         }
     }
 }
